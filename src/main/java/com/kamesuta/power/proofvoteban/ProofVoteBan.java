47aca478501f66b9eace90b6e67684056e1b9a37
yuu_111 <yuu210yuu@gmail.com>
2022-04-29 23:10:52 +0900

    commit

--- src/main/java/com/kamesuta/power/proofvoteban/ProofVoteBan.java
+++ src/main/java/com/kamesuta/power/proofvoteban/ProofVoteBan.java
@@ -1,17 +1,19 @@
 package com.kamesuta.power.proofvoteban;
 
+import dev.kotx.flylib.FlyLib;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ProofVoteBan extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        FlyLib.create(this, builder -> {
+            builder.command(new VoteBAN());
+        });
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
 }
